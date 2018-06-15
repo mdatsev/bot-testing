7269494867e3035b6960ef5097d9a45fe56edc15
mdatsev <martin.datsev.p@gmail.com>
2018-06-15 16:38:26 +0300

    using swarm secrets and some refactoring

--- gh-bot/handler.js
+++ gh-bot/handler.js
@@ -1,66 +1,80 @@
+const fs = require("fs")
+const util = require("util")
 const crypto = require("crypto")
 const rp = require("request-promise-native")
 const octokit = require("@octokit/rest")
 const gh = octokit()
 
-const github_auth_token = process.env.auth_token;
-const github_webhook_secret = process.env.webhook_secret;
-
-gh.authenticate({
-    type: "token",
-    token: github_auth_token
-})
-
-function verifyWebhook(signature, payload, secret) {
-    const hash = crypto.createHmac("sha1", secret)
-                       .update(payload)
-                       .digest("hex");
-    return crypto.timingSafeEqual(Buffer.from(signature), 
-                                  Buffer.from(`sha1=${hash}`));
+async function getSecret(name)
+{
+    const readFile = util.promisify(fs.readFile)
+    return (await readFile(`/run/secrets/${name}`)).toString()
 }
 
 function getHeader(header)
 {
-    return process.env[`Http_${header.replace('-', '_')}`]
+    return process.env[`Http_${header.replace(/-/g, '_')}`]
 }
 
-module.exports = async (context, callback) => {
-    try {
-        
-        const signature_header = getHeader("X-Hub-Signature");
-        if(!verifyWebhook(signature_header, context, github_webhook_secret))
-            throw new Error("Invalid signature");
-        
-        const event_header = getHeader("X-Github-Event");
-        if (event_header != "issues")
-            return callback("Unsupported event: " + event_header);
+function verifyWebhook(signature, payload, secret) {
+    const hash = crypto.createHmac("sha1", secret)
+                       .update(payload)
+                       .digest("hex")
+    return crypto.timingSafeEqual(Buffer.from(signature), 
+                                  Buffer.from(`sha1=${hash}`))
+}
 
-        /** @type {GitHubApiIssuesEvent} */
-        const event = JSON.parse(context);
-        const issue = event.issue;
+async function handler(request) {
+    if (!verifyWebhook(
+        getHeader("X-Hub-Signature"),
+        request,
+        await getSecret("webhook-secret")))
+        throw new Error("Invalid signature")
 
-        const sentiment_response = await rp.post("http://gateway:8080/function/sentimentanalysis", {
-            body: `${issue.title}. ${issue.body}`
-        })
-        /** @type {SentimentAnalysisApiReponse} */
-        const sentiment = JSON.parse(sentiment_response);
+    const event_header = getHeader("X-Github-Event")
+    if (event_header != "issues")
+        throw new Error(`Unrecognized or unsupported event header: ${event_header}`)
 
-        let polarity_label = 
-            sentiment.polarity > 0 
-                ? "positive" 
-                : sentiment.polarity < 0 
-                    ? "negative" 
-                    : "neutral";
-                    
-        const result = await gh.issues.replaceAllLabels({
-            owner: event.repository.owner.login,
-            repo: event.repository.name,
-            number: issue.number,
-            labels: [polarity_label]
-        });
+    /** @type {GitHubApiIssuesEvent} */
+    const event = JSON.parse(request)
+    const issue = event.issue
 
-        callback(undefined, result);
-    } catch (e) {
-        callback(e)
+    const sentiment_response = await rp.post("http://gateway:8080/function/sentimentanalysis", {
+        body: `${issue.title}. ${issue.body}`
+    })
+    /** @type {SentimentAnalysisApiReponse} */
+    const sentiment = JSON.parse(sentiment_response)
+
+    let polarity_label = 
+        sentiment.polarity > 0 
+            ? "positive" 
+            : sentiment.polarity < 0 
+                ? "negative" 
+                : "neutral"
+
+    gh.authenticate({
+        type: "token",
+        token: await getSecret("auth-token")
+    })
+    const result = await gh.issues.replaceAllLabels({
+        owner: event.repository.owner.login,
+        repo: event.repository.name,
+        number: issue.number,
+        labels: [polarity_label]
+    })
+    return result
+}
+
+module.exports = async (request, callback) => {
+    let result
+    try
+    {
+        result = await handler(request)
     }
+    catch(err)
+    {
+        return callback(err)
+    }
+    if(result)
+        return callback(undefined, result)
 }
